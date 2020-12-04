c39dc899b10f103254ec1b8684b214e8dad7901a
Mohamed Enlneily <mohamed.nele@gmail.com>
2020-12-04 02:14:22 +0000

    new database connection

--- api/config/index.js
+++ api/config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   SECRET: '32876qihsdh76@&#!742(*#HG&#28702y&##@^!()(&^#))jhscbd',
-  MONGO_URI:'mongodb://event-planner:event-planner1234@ds123465.mlab.com:23465/event-planner'
+  //MONGO_URI:'mongodb://event-planner:event-planner1234@ds123465.mlab.com:23465/event-planner'
+  MONGO_URI:'mongodb+srv://test:bUpRNUH2RWSl8DRW@event-planner.0b27a.mongodb.net/event-planner?retryWrites=true&w=majority'
 };
