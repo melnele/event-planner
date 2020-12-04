7169eebf312437823bbcf62358b766592bc52619
Mohamed Enlneily <mohamed.nele@gmail.com>
2020-12-04 02:14:57 +0000

    Update DBConnection.js

--- api/config/DBConnection.js
+++ api/config/DBConnection.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose'),
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology', true);
 // CAPTURE APP TERMINATION / RESTART EVENTS
 // To be called when process is restarted or terminated
 var gracefulShutdown = function (callback) {
